d6e7af32a8efb4cb0df18a5c11b32e6abbca5dd6
DongJaeKim <k_dongja@encs.concordia.ca>
2019-03-11 23:37:45 -0400

    learned about adding a new component

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({    
+    selector:'app-post-create',
+    templateUrl: './post-create.component.html',
+  
+})
+export class PostCreateComponent
+{
+    // a typescrip class, but turn into component by using decorator
+    
+}
